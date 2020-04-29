4e273121f6ee585e9a74d3d4ca55dd1ff776f412
psialedev <psialedev@gmail.com>
2020-04-29 15:39:15 -0500

    new migration with user,  username and usernameid fields for the User model

--- app/controllers/tweeets_controller.rb
+++ app/controllers/tweeets_controller.rb
@@ -1,6 +1,6 @@
 class TweeetsController < ApplicationController
   before_action :set_tweeet, only: [:show, :edit, :update, :destroy]
-
+  before_action :aunthenticate_user!, except: [:index, :show]
   # GET /tweeets
   # GET /tweeets.json
   def index
